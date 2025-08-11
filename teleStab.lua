da7b043ff9e8be4e8f9028263d9b1e5fb9a6b0ba
MakeMyScriptsWork <arcurush4x@gmail.com>
2025-08-10 21:50:51 -0500

    Update teleStab.lua

--- teleStab.lua
+++ teleStab.lua
@@ -1,4 +1,4 @@
-local targetPartialName = "jacmac1917"  -- Replace with partial or full player name (case-insensitive)
+local targetPartialName = "Nolan"  -- Replace with partial or full player name (case-insensitive)
 
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
@@ -200,4 +200,5 @@ UserInputService.InputBegan:Connect(function(input, gameProcessed)
     end
 end)
 print("Keybind set for F key.")
+
 
